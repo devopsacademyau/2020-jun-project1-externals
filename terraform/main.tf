2f7564a80fef8b5d59fbc5236fe853025e7bf1ef
raghunadhpokkalath <raghunadhp@gmail.com>
2020-07-25 17:58:03 +1000

    2020-jun-project1-externals/Issue#5 added ECS module

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,3 +3,9 @@ module "networking" {
   yourhomenetworkip = var.yourhomenetworkip
   vpc_cidr          = var.vpc_cidr
 }
+
+module "ecs" {
+  source            = "./modules/ecs"
+  project_name      =  var.project_name
+  vpc_id            = module.networking.vpc_id
+}
---
+++ terraform/modules/ecs/variables.tf
@@ -0,0 +1,8 @@
+variable "project_name" {
+  type        = string
+  description = "Project Name"
+}
+variable "vpc_id" {
+  type = string
+  description = "VPC ID"
+}
