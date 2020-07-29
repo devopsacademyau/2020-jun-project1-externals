849383d98a4bb3beb0b36f9965180ce21f9ee3ed
Daniel Thee Roperto <daniel@theeroperto.com>
2020-07-29 19:02:19 +1000

    Issue #29 - Using "count" in subnets

--- terraform/modules/networking/subnets.tf
+++ terraform/modules/networking/subnets.tf
@@ -1,47 +1,32 @@
 data "aws_availability_zones" "available" {}
 
-resource "aws_subnet" "public_a" {
+
+locals {
+  availability_zones_count = 2
+}
+
+resource "aws_subnet" "public" {
+  count = local.availability_zones_count
+
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.1.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[0]
-  map_public_ip_on_launch = true
+  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = false # Each resource must specifically request a public IP.
 
   tags = {
-    Name = "wp_publicsubnet_az_a"
+    Name = "wp_publicsubnet_${data.aws_availability_zones.available.names[count.index]}"
   }
 }
 
-resource "aws_subnet" "public_b" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.2.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[1]
-  map_public_ip_on_launch = true
+resource "aws_subnet" "private" {
+  count = local.availability_zones_count
 
-  tags = {
-    Name = "wp_publicsubnet_az_b"
-  }
-}
-
-resource "aws_subnet" "private_a" {
   vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.3.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[0]
+  cidr_block              = cidrsubnet(aws_vpc.this.cidr_block, 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = false
 
   tags = {
-    Name = "wp_privatesubnet_az_a"
+    Name = "wp_publicsubnet_${data.aws_availability_zones.available.names[count.index]}"
   }
 }
-
-# Subnet (private)
-resource "aws_subnet" "private_b" {
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = "10.0.4.0/24"
-  availability_zone       = data.aws_availability_zones.available.names[1]
-  map_public_ip_on_launch = false
-
-  tags = {
-    Name = "wp_privatesubnet_az_b"
-  }
-}
-
